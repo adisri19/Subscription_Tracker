44b36fd1eed290fd9e02ce06f58635854cf9b269
Aditya Srivastava <adityasrivastava9454@gmail.com>
2026-04-24 04:07:13 +0530

    Integrate Arcjet and protect user routes

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errormiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
--- config/env.js
+++ config/env.js
@@ -4,4 +4,5 @@ config({
   path: `.env.${process.env.NODE_ENV || "development"}.local`,
 });
 
-export const { PORT , NODE_ENV , DB_URL, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
+export const { 
+  PORT , NODE_ENV , DB_URL, JWT_SECRET, JWT_EXPIRES_IN, ARCJET_API_KEY, ARCJET_ENV } = process.env;
